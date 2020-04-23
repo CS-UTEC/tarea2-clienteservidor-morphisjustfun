5f191808efaf56b5c7c9dd7e230ca994b631595b
morphisjustfun <mariojacobo2011@gmail.com>
2020-04-23 16:06:21 -0500

    Clase 23/04 - Database

--- web/server.py
+++ web/server.py
@@ -8,6 +8,7 @@ db = connector.Manager()
 engine = db.createEngine()
 
 app = Flask(__name__)
+
 @app.route('/palindrome/<palabra>')
 def palindrome(palabra):
     i =int(len(palabra)/2)
@@ -34,7 +35,36 @@ def esmultiplo(numero1,numero2):
 def static_content(content):
     return render_template(content)
 
+@app.route('/create_user/<nombre>/<apellido>/<passwd>/<nick>')
+def create_user(nombre,apellido,passwd,nick):
+    #crear un objeto (instancia de una clase)
+    user = entities.User(
+        name = nombre,
+        fullname = apellido,
+        password = passwd,
+        username = nick
+    )
 
+    #Guardar el objeto en la capa decreate_user persistencia
+    db_session = db.getSession(engine)
+    db_session.add(user)
+    db_session.commit()
+    return "User created!"
+
+@app.route('/read_user')
+def read_users():
+    db_session = db.getSession(engine)
+    respuesta = db_session.query(entities.User)
+    users = respuesta[:]
+    i = 0
+    final = ""
+    for user in users:
+        if (i==0):
+            final = final + str(i) + " Nombre: " + user.name + " Apellido: " + user.fullname + " Pass: " + user.password + " Username: " + user.username
+        else:
+            final = final + "<br/>" +str(i) + " Nombre: " + user.name + " Apellido: " + user.fullname + " Pass: " + user.password + " Username: " + user.username
+        i+=1
+    return final
 
 
 if __name__ == '__main__':
